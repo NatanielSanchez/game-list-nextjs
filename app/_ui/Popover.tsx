0500b1761dd8cbe787dfa7c82a9147cd116daa51
NatanielSanchez <natanielsanchez2077@gmail.com>
2025-10-28 16:54:45 -0300

    renamed Popover inner components, fixed styling

--- app/_ui/Popover.tsx
+++ app/_ui/Popover.tsx
@@ -3,30 +3,27 @@ import { createContext, Dispatch, PropsWithChildren, ReactNode, SetStateAction, 
 import { useOutsideClick } from "../_hooks/useOutsideClick";
 import { useKey } from "../_hooks/useKey";
 import { createPortal } from "react-dom";
-import useIsMounted from "../_hooks/useIsMounted";
 import styles from "@/app/_styles/Popover.module.scss";
-import { useTheme } from "next-themes";
 
 type Position = { x: number; y: number };
 type MenusState = { openMenuId: string; position: Position };
-type MenusContextValue = MenusState & {
+type PopoverContextValue = MenusState & {
   open: (id: string) => void;
   close: () => void;
   setPosition: Dispatch<SetStateAction<Position>>;
 };
-const MenusContext = createContext<MenusContextValue | null>(null);
+const PopoverContext = createContext<PopoverContextValue | null>(null);
 
 function Popover({ children }: { children: ReactNode }) {
-  const isMounted = useIsMounted();
-
   const [openMenuId, setOpenMenuId] = useState("");
   const [position, setPosition] = useState<Position>({ x: 0, y: 0 });
   const open = setOpenMenuId;
   const close = () => setOpenMenuId("");
 
-  if (!isMounted) return null;
   return (
-    <MenusContext.Provider value={{ openMenuId, open, close, position, setPosition }}>{children}</MenusContext.Provider>
+    <PopoverContext.Provider value={{ openMenuId, open, close, position, setPosition }}>
+      {children}
+    </PopoverContext.Provider>
   );
 }
 
@@ -52,18 +49,13 @@ function Toggle({ id, renderItem }: { id: string; renderItem: (onClick: (e: Reac
 }
 
 function Content({ id, children }: PropsWithChildren<{ id: string }>) {
-  const { theme } = useTheme();
   const { openMenuId, position, close } = useMenus();
   const ref = useOutsideClick<HTMLDivElement>(close, false); // close on bubbling phase
   useKey("escape", close);
 
   if (openMenuId !== id) return null;
   return createPortal(
-    <div
-      className={`${styles.content} ${styles[theme === "dark" ? "dark" : "light"]}`}
-      ref={ref}
-      style={{ right: `${position.x}px`, top: `${position.y}px` }}
-    >
+    <div className={styles.content} ref={ref} style={{ right: `${position.x}px`, top: `${position.y}px` }}>
       {children}
     </div>,
     document.body
@@ -75,7 +67,7 @@ Popover.Content = Content;
 export default Popover;
 
 function useMenus() {
-  const context = useContext(MenusContext);
-  if (!context || context === null) throw new Error("MenusContext was accessed outside of provider.");
+  const context = useContext(PopoverContext);
+  if (!context || context === null) throw new Error("PopoverContext was accessed outside of provider.");
   return context;
 }
